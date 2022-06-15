b9947393d56b40e91eb4bf44bd4becb465d8bd5e
Anastasia Platunova <Anastasia@MacBook-Air.local>
2022-06-15 13:53:15 +0300

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $(".lang-en").click(function () {
+        $(".lang-en").toggleClass("active");
+    })
+    $(function() {
+        $('.tabs nav a').on('click', function() {
+            show_content($(this).index());
+        });
+
+        show_content(0);
+
+        function show_content(index) {
+            // Make the content visible
+            $('.tabs .content.visible').removeClass('visible');
+            $('.tabs .content:nth-of-type(' + (index + 1) + ')').addClass('visible');
+
+            // Set the tab to selected
+            $('.tabs nav a.selected').removeClass('selected');
+            $('.tabs nav a:nth-of-type(' + (index + 1) + ')').addClass('selected');
+        }
+    });
+});
